dd69baa121c03d93269434a2fb23d8d95c442047
Prashanthimallela <119045241+Prashanthimallela@users.noreply.github.com>
2022-12-30 18:13:23 +0530

    Add files via upload

---
+++ LoginDao.java
@@ -0,0 +1,14 @@
+package com.training.web.dao;
+
+import java.util.List;
+
+import com.training.web.model.User;
+
+import comm.assignment.web.model.Product;
+
+
+public  interface LoginDao {
+	
+	 List<User> getUsers();	
+
+}
---
+++ LoginDaoImpl.java
@@ -0,0 +1,54 @@
+package com.training.web.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.training.web.model.User;
+
+public class LoginDaoImpl implements LoginDao{
+
+	@Override
+	public List<User> getUsers() {
+		
+		 List<User> userList = new ArrayList<>();
+		 Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		 
+	        //Scanner sc = new Scanner(System.in);
+	        String url = "jdbc:mysql://localhost:3306/infinite";
+	        String user = "root";
+	        String pass = "root@123";
+	        con = DriverManager.getConnection(url,user,pass);
+	        Statement stmt = con.createStatement();
+	        ResultSet rs = stmt.executeQuery("select * from username");
+	        while(rs.next()) {
+	          	userList.add(new User(rs.getString(1), rs.getString(2)));
+	        	//userList.add(new User("ram","ram@123"));
+	        	
+	        }
+	       
+		 }  
+	        catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  
+		finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return userList;
+	}}
+
+
